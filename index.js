e0e95b501d2f5386981893444d75d855d0dba31b
Tayler Schanks <taylerschanks2@gmail.com>
2023-05-22 11:55:24 -0500

    added images and some javascript

--- index.js
+++ index.js
@@ -1,5 +1,52 @@
 document.addEventListener("DOMContentLoaded", () => {
-    fetch(" https://fishbase.ropensci.org/species")
+    fetch("https://swapi.dev/api/people/")
         .then(resp => resp.json())
-        .then(json => console.log(json))
-})
+        .then(json => console.log(json));
+});
+
+let arrayOfImages = [
+    {
+        name: "Luke Skywalker",
+        image: "Images/Luke Skywalker.jpg"
+    },
+    {
+        name: "C-3PO",
+        image: "Images/C-3PO.jpg"
+    },
+    {
+        name: "R2-D2",
+        image: "Images/R2-D2.jpeg"
+    },
+    {
+        name: "Darth Vader",
+        image: "Images/Darth Vader.jpg"
+    },
+    {
+        name: "Leia Organa",
+        image: "Images/Leia Organa.jpeg"
+    },
+    {
+        name: "Obi-Wan Kenobi",
+        image: "Images/Obi-Wan Kenobi.jpeg"
+    }
+];
+
+function addImage(json) {
+    let people = json.results;
+
+    people.forEach(obj => {
+        arrayOfImages.forEach(imageObj => {
+            if (obj.name === imageObj.name) {
+                obj.image = imageObj.image;
+            }
+        });
+    });
+
+    console.log(people);
+}
+
+function loadCharacter(json) {
+    let people = json.results;
+    addImage(json);
+
+}
