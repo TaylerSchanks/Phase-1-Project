c51452836697aa91deb7a962c96f7f27262fb5a6
Tayler Schanks <taylerschanks2@gmail.com>
2023-05-22 13:38:06 -0500

    adding image elements

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
     fetch("https://swapi.dev/api/people/")
         .then(resp => resp.json())
-        .then(json => console.log(json));
+        .then(json => loadCharacter(json));
 });
 
 let arrayOfImages = [
@@ -26,6 +26,22 @@ let arrayOfImages = [
         image: "Images/Leia Organa.jpeg"
     },
     {
+        name: "Owen Lars",
+        image: "Images/Owen Lars.jpg"
+    },
+    {
+        name: "Beru Whitesun lars",
+        image: "Images/Beru Whitesun lars.jpg"
+    },
+    {
+        name: "R5-D4",
+        image: "Images/R5-D4.jpeg"
+    },
+    {
+        name: "Biggs Darklighter",
+        image: "Images/Biggs Darklighter.jpeg"
+    },
+    {
         name: "Obi-Wan Kenobi",
         image: "Images/Obi-Wan Kenobi.jpeg"
     }
@@ -46,7 +62,34 @@ function addImage(json) {
 }
 
 function loadCharacter(json) {
-    let people = json.results;
     addImage(json);
+    let people = json.results;
+    const characterList = document.querySelector("#Character-List")
+    const lukeSkywalkerElement = document.createElement('img')
+    const c3poElement = document.createElement('img')
+    const r2d2Element = document.createElement('img')
+    const darthVaderElement = document.createElement('img')
+    const leiaOrganaElement = document.createElement('img')
+    const owenLarsElement = document.createElement('img')
+    const beruWhitesunLarsElement = document.createElement('img')
+    const r5d4Element = document.createElement('img')
+    const biggsDarklighterElement = document.createElement('img')
+    const obiWanKenobiElement = document.createElement('img')
+    characterList.appendChild(lukeSkywalkerElement)
+    characterList.appendChild(c3poElement)
+    characterList.appendChild(r2d2Element)
+    characterList.appendChild(darthVaderElement)
+    characterList.appendChild(leiaOrganaElement)
+    characterList.appendChild(owenLarsElement)
+    characterList.appendChild(beruWhitesunLarsElement)
+    characterList.appendChild(r5d4Element)
+    characterList.appendChild(biggsDarklighterElement)
+    characterList.appendChild(obiWanKenobiElement)
+
+
+    const lukeSkyWalkerImage = "Images/Luke Skywalker.jpg"
+
+
+    lukeSkywalkerElement.setAttribute('src', lukeSkyWalkerImage)
 
 }
